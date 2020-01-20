a0d667e441f0d288b058210ba99ba76b69333b97
Mark Anthony Lumbao <mark.lumbao@extendbpo.local>
2020-01-21 06:43:29 +0800

    Added client-side fetch script

--- web-server/public/src/script.js
+++ web-server/public/src/script.js
@@ -1,1 +1,8 @@
 console.log('Weather app coming soon!');
+
+fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
+    response.json().then((data) => {
+        if(data.error){ return console.log(data.error); }
+        console.log(`${data.location} \n ${data.forecast}`);
+    });
+});
