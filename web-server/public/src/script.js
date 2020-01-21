1cd6ee7b084688ec682e864ad8fed38473f3f440
Mark Anthony Lumbao <mark.lumbao@extendbpo.local>
2020-01-21 23:00:01 +0800

    Improved weather app layout and fetch function

--- web-server/public/src/script.js
+++ web-server/public/src/script.js
@@ -1,8 +1,34 @@
 console.log('Weather app coming soon!');
 
-fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
-    response.json().then((data) => {
-        if(data.error){ return console.log(data.error); }
-        console.log(`${data.location} \n ${data.forecast}`);
-    });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const forecastResultTitle = document.querySelector('#forecast-result').querySelector('h3');
+const forecastResult = document.querySelector('#forecast-result').querySelector('p');
+const errorMessage = document.querySelector('#error-message');
+
+weatherForm.addEventListener('submit',(event) => {
+    // prevents the form from running its default event 
+    //which refreshes the whole page when submitted
+    event.preventDefault(); 
+
+    const address = search.value;
+    
+    if(address){
+        errorMessage.textContent = '';
+        forecastResultTitle.textContent = 'Loading...';
+        forecastResult.textContent = '';
+
+        fetch('http://localhost:3000/weather?address='+address).then((response)=>{
+            response.json().then((data) => {
+
+                if(data.error){ 
+                    errorMessage.textContent = data.error;
+                    forecastResultTitle.textContent = '';
+                    return;
+                }
+                forecastResultTitle.textContent = data.location;
+                forecastResult.textContent = data.forecast;
+            });
+        });
+    }
 });
