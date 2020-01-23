6b9a3aac5bac0cc4478d33136d6feba1428ee8fe
Mark Anthony Lumbao <mark.lumbao@extendbpo.local>
2020-01-24 02:59:42 +0800

    added async-await sample

---
+++ playground/async-await.js
@@ -0,0 +1,18 @@
+const add = (a,b) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            if(a<0||b<0){ return reject('Numbers must be positive.') }
+            resolve(a+b);
+        },2000);
+    })
+}
+
+const doWork = async () => {
+    const sum = await add(1,2);
+    const sum2 = await add(sum,21);
+    const sum3 = await add(sum2,41);
+    return sum3;
+}
+
+doWork().then(result => console.log(result))
+.catch(error => console.log(error) );
